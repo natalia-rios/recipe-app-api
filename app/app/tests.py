a3f76f2f63d67af8c009f57d21a27b266a9bca6c
riosnatalia <natalia.rios@wexinc.com>
2022-05-30 17:55:59 -0300

    linting files

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,6 +6,7 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
 
@@ -14,9 +15,8 @@ class CalcTests(SimpleTestCase):
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
 
-
     def test_substract_numbers(self):
         """Test subtracting numbers."""
         res = calc.subtract(10, 15)
-        
+
         self.assertEqual(res, 5)
